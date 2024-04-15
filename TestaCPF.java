e300a34dd55f8146f351ceb32e43df1bc6f044f0
Isadora Caetano <142831997+isacbs@users.noreply.github.com>
2024-04-15 15:26:27 -0300

    Add files via upload

---
+++ TestaCPF.java
@@ -0,0 +1,22 @@
+package Lab2;
+
+import java.util.Scanner;
+
+public class TestaCPF {
+	    public static void main(String[] args) {
+	        Scanner ent = new Scanner(System.in);
+
+	        System.out.println("Digite o número do CPF na forma XXX.XXX.XXX-XX : ");
+	        String teste = ent.nextLine();
+
+	        CPF cpf = new CPF();
+	        cpf.cadastrar(teste);
+
+	        if (cpf.validar()) 
+	            System.out.println("CPF válido: " + cpf.formatar());
+	        else 
+	            System.out.println("CPF inválido: " + cpf.formatar());
+
+	        ent.close();
+	    }
+}

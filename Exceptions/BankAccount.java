9225c143f8cfd855cf9ff0bc5d2fd7c474221861
Isadora Caetano <142831997+isacbs@users.noreply.github.com>
2024-08-20 08:10:29 -0300

    BankAccount.java

--- Exceptions/BankAccount.java
+++ Exceptions/BankAccount.java
@@ -1,48 +1,50 @@
 package Exceptions;
 
 public class BankAccount {
-    private String accountNumber;
-    private double balance;
-    private String accountHolderName;
+	private int accountNumber;
+	private double balance;
+	private String accountHolderName;
 
-    public BankAccount(String accountNumber, String accountHolderName) {
-        this.accountNumber = accountNumber;
-        this.accountHolderName = accountHolderName;
-        this.balance = 0.0; 
-    }
+	public BankAccount(int accountNumber, String accountHolderName, double balance) {
+		this.accountNumber = accountNumber;
+		this.accountHolderName = accountHolderName;
+		this.balance = balance;
+	}
 
-    // Método para Depósito
-    public void deposit(double valor) {
-        if (valor < 0) {
-            throw new IllegalArgumentException("Valor do depósito não pode ser menor que 0!!!");
-        }
-        balance += valor;
-        System.out.println("Depósito de " + valor + " reais realizado com sucesso. O valor do saldo atual é R$ " + balance  + ".");
-    }
+	// Método para Depósito
+	public void deposit(double valor) {
+		if (valor < 0) {
+			throw new IllegalArgumentException("O valor para depósitos não pode ser negativo!! \nDepósito invalidado de R$" + valor  + ".");
+		}
+		balance += valor;
+		System.out.println(
+				"Depósito de " + valor + " reais realizado com sucesso. O valor do saldo atual é R$ " + balance + ".");
+	}
 
-    // Método para Saque
-    public void withdraw(double valor) throws InsufficientFundsException {
-        if (valor < 0) {
-            throw new IllegalArgumentException("O valor do saque não pode ser menor que 0!!!");
-        }
-        if (valor > balance) {
-            throw new InsufficientFundsException(valor, balance);
-        }
-        balance -= valor;
-        System.out.println("Saque de " + valor + " reais realizado com sucesso. O valor do saldo atual é R$ " + balance  + ".");
-    }
+	// Método para Saque
+	public void withdraw(double valor) throws InsufficientFundsException {
+		if (valor < 0) {
+			throw new IllegalArgumentException("O valor do saque não pode ser negativo!! \nSaque invalidado de R$" + valor  + ".");
+		}
+		if (valor > balance) {
+			throw new InsufficientFundsException(valor, balance);
+		}
+		balance -= valor;
+		System.out.println(
+				"Saque de " + valor + " reais realizado com sucesso. O valor do saldo atual é R$ " + balance + ".");
+	}
 
-    // Getters
-    public double getBalance() {
-        return balance;
-    }
+	// Getters
+	public double getBalance() {
+		return balance;
+	}
 
-    public String getAccountNumber() {
-        return accountNumber;
-    }
+	public int getAccountNumber() {
+		return accountNumber;
+	}
 
-       public String getAccountHolderName() {
-        return accountHolderName;
-    }
-       
+	public String getAccountHolderName() {
+		return accountHolderName;
+	}
+
 }

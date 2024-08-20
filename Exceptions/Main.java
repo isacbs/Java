ad72ce894eaf571be4775a2de3a332a051fa5754
Isadora Caetano <142831997+isacbs@users.noreply.github.com>
2024-08-20 08:11:19 -0300

    Main.java

--- Exceptions/Main.java
+++ Exceptions/Main.java
@@ -5,16 +5,21 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner ent = new Scanner(System.in);
-		BankAccount account = new BankAccount("97543", "Isadora Caetano");
+		BankAccount account = new BankAccount(97543, "Isadora Caetano", 100);
 
 		try {
-			// Solicita ao usuário o valor do depósito
-			System.out.print("Informe o valor desejado para fazer o depósito: ");
-			double deposito = ent.nextDouble();
-			account.deposit(deposito);
+			// Exibe os dados iniciais da conta
+			System.out.println("---:: Informações da Conta ::---");
+			System.out.println("Número da conta: " + account.getAccountNumber());
+			System.out.println("Nome do titular: " + account.getAccountHolderName());
+			System.out.println("Saldo inicial: R$ " + account.getBalance());
 
-			// Solicita ao usuário o valor do saque
-			System.out.print("Informe o valor desejado para fazer o saque: ");
+			// Solicita ao usuário o valor do depósito e do saque
+			System.out.print("\nInforme o valor desejado para fazer o depósito: ");
+			double dep = ent.nextDouble();
+			account.deposit(dep);
+
+			System.out.print("\nInforme o valor desejado para fazer o saque: ");
 			double saque = ent.nextDouble();
 			account.withdraw(saque);
 
@@ -26,5 +31,4 @@ public class Main {
 
 		ent.close();
 	}
-
 }

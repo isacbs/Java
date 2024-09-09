bf2140720a49531f4a66c320dae95a2cb98bf14a
Isadora Caetano <142831997+isacbs@users.noreply.github.com>
2024-09-09 12:23:26 -0300

    TADLista.java

--- tadlista/TADLista.java
+++ tadlista/TADLista.java
@@ -2,26 +2,27 @@ package tadlista;
 
 public class TADLista {
     public static void main(String[] args) throws Exception {
-        Lista gamers = new Lista(10);
+        Lista gamers = new Lista(5);
 
-        gamers.add(new Gamer("Isadora", 840));
-        gamers.add(new Gamer("Ana", 789));
-        gamers.add(new Gamer("Deize", 325));
-        gamers.add(new Gamer("Maria", 653));
-        gamers.add(new Gamer("Vera", 641));
+        gamers.add(0, new Gamer("Isadora", 913));
+        gamers.add(1, new Gamer("Ana", 789));
+        gamers.add(2, new Gamer("Deize", 325));
+        gamers.add(3, new Gamer("Maria", 656));
+        gamers.add(4, new Gamer("Vera", 641));
 
-        
-        System.out.println("Lista inicial dos scores:");
+        gamers.set(4, new Gamer("Diana", 694));
+        gamers.remove(2);
+        gamers.add(3, new Gamer("Allie", 892));
         gamers.mostraLista();
 
-        
-        System.out.println("Jogador em primeiro lugar: " + gamers.get(0));
+        Gamer x = gamers.get(1);
+        System.out.println("Conteúdo = " + x);
 
-      
-        gamers.remove(3);
-
-        
-        System.out.println("Lista dos scores após remover o quarto jogador:");
-        gamers.mostraLista();
-    }
+        int result = gamers.search(new Gamer("Maria", 656));
+        if (result == -1) {
+            System.out.println("Não existe 'Maria' na lista");
+        } else {
+            System.out.println("Maria está na posição " + result);
+        }
+    } 
 }

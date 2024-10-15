ed4cb208b1bc18b7588a2d649cb48c5292e43643
Isadora Caetano <142831997+isacbs@users.noreply.github.com>
2024-10-15 17:48:05 -0300

    TADFila.java

--- fila/TADFila.java
+++ fila/TADFila.java
@@ -5,9 +5,9 @@ public class ProjFila {
     public static void main(String[] args) throws Exception{
         
         Queue fila1 = new Queue(10);
-        fila1.enqueue("MARIA");
-        fila1.enqueue("PEDRO");
-        fila1.enqueue("JOANA");
+        fila1.enqueue("Isadora");
+        fila1.enqueue("Ana Clara");
+        fila1.enqueue("Ellen");
         System.out.println("Primeiro da fila: " + fila1.front());
         fila1.dequeue();
         System.out.println("Primeiro da fila: " + fila1.front());

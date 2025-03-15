80914774b87bf0f3a0e993744300635eb50f9a85
Isadora Caetano <142831997+isacbs@users.noreply.github.com>
2025-03-15 16:04:23 -0300

    Cliente.java

--- Locação de bicicletas elétricas/Cliente.java
+++ Locação de bicicletas elétricas/Cliente.java
@@ -12,12 +12,21 @@ package Projeto;
 * **/
 
 public class Cliente {
-    String nome;
-    String cpf;
-    double totalGasto;
-    boolean bicicletaAlugada;
-    int quantidadeLocacoes;
+    private String nome;
+    private String cpf;
+    private double totalGasto;
+    private boolean bicicletaAlugada;
+    private int quantidadeLocacoes;
 
+    /**
+     * Construtor da classe Cliente, que inicializa os atributos 
+     *
+     * @param nome Nome do cliente;.
+     * @param cpf CPF do cliente;
+     * @param totalGasto Total gasto das locações feitas pelo cliente;
+     * @param bicicletaAlugada Checa se a bicicleta está alugada no momento;
+     * @param quantidadeLocacoes Quantidade de locações que o cliente fez.
+     */
     public Cliente(String nome, String cpf) {
         this.nome = nome;
         this.cpf = cpf;
@@ -26,6 +35,16 @@ public class Cliente {
         this.quantidadeLocacoes = 0;
     }
 
+    /**
+     * Retorna os atributos
+     * 
+     * @return nome do cliente;.
+     * @return cpf do cliente;
+     * @return total gasto das locações feitas pelo cliente;
+     * @return true se a bicicleta estiver alugada, false caso não esteja;
+     * @return quantidade de locações que o cliente fez.
+     * 
+     */
     public String getNome() {
         return nome;
     }
@@ -46,21 +65,33 @@ public class Cliente {
         return quantidadeLocacoes;
     }
 
+    /**
+     * Realiza a locação de uma bicicleta, aumentando o total gasto e a quantidade de locações.
+     * 
+     */
     public void locarBicicleta() {
         this.bicicletaAlugada = true;
         this.totalGasto += 5.00;
         this.quantidadeLocacoes++;
     }
 
+    /**
+     * Devolve a bicicleta, alterando o estado da locação.
+     * 
+     */
     public void devolverBicicleta() {
         this.bicicletaAlugada = false;
     }
 
+    /**
+     * String com as informações do cliente.
+     * 
+     */
     @Override
     public String toString() {
         return "Nome: " + nome + "\nCPF: " + cpf + "\nTotal Gasto: R$ " + totalGasto +
-               "\nBicicleta Alugada: " + (bicicletaAlugada ? "Sim" : "Não") + 
+               "\nBicicleta Alugada: " + (bicicletaAlugada ? "Sim" : "Não") +
                "\nTotal de Locações: " + quantidadeLocacoes;
     }
-
+    
 }
